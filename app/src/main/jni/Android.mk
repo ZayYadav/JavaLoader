0fdba52743a9fe6e473daff99268de28fa05eaad
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-24 02:14:27 +0530

    build: compile native production signer root

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := client
 
-LOCAL_SRC_FILES := Main.cpp SignatureIntegrity.cpp WrapperPayloadGuard.cpp NativeApkSignerBinding.cpp ApkArchiveIntegrity.cpp
+LOCAL_SRC_FILES := Main.cpp SignatureIntegrity.cpp WrapperPayloadGuard.cpp NativeApkSignerBinding.cpp ApkArchiveIntegrity.cpp PinnedProductionSigner.cpp
 
 # Only current folder include
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
